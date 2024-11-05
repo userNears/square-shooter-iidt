f3a00a468aa27bd921ad677c6e543d0df018188c
hazeka <hazeka.joeli@gmail.com>
2024-11-05 10:06:07 +0100

    test

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+pygame.init()
+
+# skapa skärmen där höjden är 900 och breden är 700
+skärm = pygame.display.set_mode((900, 700))
+
+
+
+# titeln på spelet
+pygame.display.set_caption("Pygame krig game shit duc denis shir eli shit joeli shit")
+
+run = True
+while run:
+    for pygame_event in pygame.event.get():
+        if pygame_event.type == pygame.QUIT:
+            run = False
+
+
+    #färgen på skärmen  "röd", " grön", "blå"       
+    skärm.fill((0, 0, 0))
+
+    #uppdatera skärmen
+    pygame.display.flip()
+
+pygame.quit()
