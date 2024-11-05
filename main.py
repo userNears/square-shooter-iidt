b5e98e47892c9a7572fc00fe0e7ff77fa6c86636
usernears <usernears@gmail.com>
2024-11-05 10:43:28 +0100

    fixed code

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import pygame
 from src import settings
 
-pygame.init()
+def main():
+    pygame.init()
 
-# skapa skärmen där höjden är 900 och breden är 700
-skärm = pygame.display.set_mode(settings.SCREEN_SIZE)
+    # Game title
+    pygame.display.set_caption("Python Game Project")
 
+    environment = pygame.display.set_mode(settings.SCREEN_SIZE)
+    
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
 
+        # Update environment
+        environment.fill(settings.BACKGROUND_COLOR) # maybe this shouldnt be here but outside the while loop
+        pygame.display.flip()
 
-# titeln på spelet
-pygame.display.set_caption("Pygame krig game shit duc denis shir eli shit joeli shit")
-
-run = True
-while run:
-    for pygame_event in pygame.event.get():
-        if pygame_event.type == pygame.QUIT:
-            run = False
-
-
-    #färgen på skärmen  "röd", " grön", "blå"       
-    skärm.fill((0, 0, 0))
-
-    #uppdatera skärmen
-    pygame.display.flip()
-
-pygame.quit()
+if __name__ == "__main__":
+    main()
